8d6eb8c2cf5201ebd8139400262201b0e31ddf5c
prohorp <usoltsev05@mail.ru>
2024-08-31 10:16:22 +0800

    01

--- 01_hello/hello.c
+++ 01_hello/hello.c
@@ -1,17 +1,20 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-static unsigned long init_number = 100;
+static unsigned long init_number = 1;
+static char* whom = "world";
 
 static int __init hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	for (size_t i = 0; i < init_number; i++)
+		printk(KERN_ALERT "Hello, %s\n", whom);
+
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world: %ld\n", init_number);
+	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
 module_init(hello_init);
@@ -19,6 +22,7 @@ module_exit(hello_exit);
 
 /* Set desired capacity with insmod */
 module_param_named(number, init_number, ulong, S_IRUGO);
+module_param(whom, charp, S_IRUGO);
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("Simple hello driver");
