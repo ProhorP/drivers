78e6c02fe5109910fa0dec1f93f9d5a74b430792
prohorp <usoltsev05@mail.ru>
2024-09-09 20:10:21 +0800

    03_proc_read

--- 03_proc_read/README.md
+++ 03_proc_read/README.md
@@ -1,8 +1,10 @@
 Задание:  
-Создать модуль ядра, который при регистрации сохранит строку, которую нужно вывести при вызове /proc/proc_read
+Создать модуль ядра, который создаст /proc/proc_read  
+В него можно будет писать и читать из него строки
 ```
 make
-sudo insmod proc_read.ko str="Hello"
+sudo insmod proc_read.ko
+echo 'Hello proc file!' > /proc/proc_read
 cat /proc/proc_read
 sudo rmmod proc_read.ko
 make clean
--- 03_proc_read/proc_read.c
+++ 03_proc_read/proc_read.c
@@ -2,38 +2,45 @@
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 
-static char* str = "Non";
+static char msg[100];
+static struct proc_dir_entry *proc_file;
 
-int proc_read(char* buf, char** start, off_t offset, int count, int* eof,
-              void* data)
-{
-	int len = 0;
-	len = sprintf(buf + len, "%s\n", str);
-	*eof = 1;
-	return len;
+ssize_t my_proc_read(struct file *filp, char __user *buffer, size_t len, loff_t *offset) {
+    return simple_read_from_buffer(buffer, len, offset, msg, strlen(msg));
 }
 
-static int __init proc_read_init(void)
-{
-	create_proc_read_entry("proc_read", 0 /* режим по умолчанию */,
-	                       NULL /* родительская директория */,
-	                       proc_read /* функция для вызова*/,
-	                       NULL /* клиентские данные */);
+ssize_t my_proc_write(struct file *filp, const char __user *buffer, size_t len, loff_t *offset) {
+    if (len > sizeof(msg) - 1)
+        return -EINVAL;
 
-	return 0;
+    if (copy_from_user(msg, buffer, len))
+        return -EFAULT;
+
+    msg[len] = '\0';
+    return len;
+}
+
+static const struct proc_ops proc_fops = {
+    .proc_read = my_proc_read,
+    .proc_write = my_proc_write,
+};
+
+static int __init proc_read_init(void) {
+	memset(msg, 0, sizeof(msg));
+    proc_file = proc_create("proc_read", 0666, NULL, &proc_fops);
+    if (!proc_file) {
+        return -ENOMEM;
+    }
+    return 0;
 }
 
 static void __exit proc_read_exit(void)
 {
-
 	remove_proc_entry("proc_read", NULL /* родительская директория */);
 }
 
 module_init(proc_read_init);
 module_exit(proc_read_exit);
-
-/* Set desired capacity with insmod */
-module_param(str, charp, S_IRUGO);
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("Proc read file");
